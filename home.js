25a39300d1510850972e74c8f9577e734650f3d7
NaFaPaz <nfanego@gmail.com>
2022-12-29 01:07:49 -0300

    [add] filters

--- home.js
+++ home.js
@@ -1,58 +1,93 @@
-function filledTemplate(image, name, description, price) {
+function fillCard(event) {
   return `
       <div class="card" style="width: 18rem">
           <img
-            src="${image}"
+            src="${event.image}"
             class="card-img-top"
-            alt="${name}"
+            alt="${event.name}"
           />
-          <h5 class="card-title">${name}</h5>
+          <h5 class="card-title">${event.name}</h5>
           <div class="card-body">
-            <p class="card-text">${description}</p>
+            <p class="card-text">${event.description}</p>
             <div
               class="card-bottom d-flex justify-content-between align-items-center"
             >
-              <p>Price $${price}</p>
+              <p>Price $${event.price}</p>
               <a href="./details.html" class="btn btn-primary">View more</a>
             </div>
           </div>
         </div>
         `;
 }
-function cardTemplate(cardsData, filter) {
-  let template = '';
+function cardEvents(cardsData, filter) {
+  let events = [];
   for (let card of cardsData) {
     if (filter === 'past') {
       if (card.date < data.currentDate) {
-        template += filledTemplate(
-          card.image,
-          card.name,
-          card.description,
-          card.price
-        );
+        events.push(card);
       }
     } else if (filter === 'upcoming') {
       if (card.date > data.currentDate) {
-        template += filledTemplate(
-          card.image,
-          card.name,
-          card.description,
-          card.price
-        );
+        events.push(card);
       }
     } else {
-      template += filledTemplate(
-        card.image,
-        card.name,
-        card.description,
-        card.price
-      );
+      events.push(card);
     }
   }
-  return template;
+  return events;
+}
+function showTemplate(filteredEvents) {
+  let template = [];
+  for (let event of filteredEvents) {
+    template.push(fillCard(event));
+  }
+  return template.toString();
 }
 let cards = document.querySelector('.cards');
-const templateHome = cardTemplate(data.events);
-const templatePast = cardTemplate(data.events, 'past');
-const templateUpcoming = cardTemplate(data.events, 'upcoming');
-cards.innerHTML = templateHome;
+let eventsCards = cardEvents(data.events);
+cards.innerHTML = showTemplate(eventsCards);
+
+//filters
+//search
+function searchEvents(events, query) {
+  let results = [];
+  for (let event of events) {
+    if (event.name.toLowerCase().includes(query)) {
+      results.push(event);
+    }
+  }
+  return results;
+}
+let searchBar = document.querySelector('input[type="search"]');
+searchBar.addEventListener('input', (e) => {
+  let query = e.target.value.toLowerCase().trim();
+  let resultsTemplate = searchEvents(eventsCards, query);
+  cards.innerHTML = showTemplate(resultsTemplate);
+});
+function checksFilter(events, checks) {
+  let filtered = [];
+  for (let event of events) {
+    if (checks.includes(event.category.toLowerCase().replace(' ', '-'))) {
+      filtered.push(event);
+    }
+  }
+  return filtered;
+}
+let checks = document.querySelectorAll('input[type="checkbox"]');
+let activeChecks = [];
+checks.forEach((check) =>
+  check.addEventListener('change', (e) => {
+    let checkID = e.target.id;
+    if (activeChecks.includes(checkID)) {
+      activeChecks = activeChecks.filter((check) => check !== checkID);
+    } else {
+      activeChecks.push(checkID);
+    }
+    let filtersTemplate = checksFilter(eventsCards, activeChecks);
+    if (filtersTemplate.length > 0) {
+      cards.innerHTML = showTemplate(filtersTemplate);
+    } else {
+      cards.innerHTML = showTemplate(eventsCards);
+    }
+  })
+);
